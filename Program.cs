16f2fefaabe5e97c0a5fefb7dda95a5eae410a36
jason <jason.meisel@gmail.com>
2019-06-08 02:51:43 -0700

    getting close!

--- Program.cs
+++ Program.cs
@@ -122,6 +122,11 @@ public static class Actions
         return () => $"Add({valueA()}, {valueB()})";
     }
 
+    public static LazyString Subtract(LazyString valueA, LazyString valueB)
+    {
+        return () => $"Subtract({valueA()}, {valueB()})";
+    }
+
     public static LazyString NotEqual(LazyString a, LazyString b)
     {
         return () => $"Compare({a()}, !=, {b()})";
@@ -133,50 +138,64 @@ public static class Actions
     }
 }
 
-namespace IL2Workshop
+
+class Program
 {
 
-    class Program
+    public static LazyString[] ToWorkshopActions(MethodDefinition method, Instruction instruction)
     {
+        if (instruction.OpCode == OpCodes.Ldarg_0)
+            return PushToVariableStack(GetLastElementOfParameterStack(0));
+        if (instruction.OpCode == OpCodes.Ldarg_1)
+            return PushToVariableStack(GetLastElementOfParameterStack(1));
+        if (instruction.OpCode == OpCodes.Ldarg_2)
+            return PushToVariableStack(GetLastElementOfParameterStack(2));
+        if (instruction.OpCode == OpCodes.Ldarg_3)
+            return PushToVariableStack(GetLastElementOfParameterStack(3));
 
-        public static LazyString[] ToWorkshopActions(Instruction instruction)
+        if (instruction.OpCode == OpCodes.Add)
+            return DoBinaryOp(Add);
+
+        // TODO: pop parameters off stack and loop (put abort-if-false at top of function)
+        if (instruction.OpCode == OpCodes.Ret)
+            return new LazyString[] { () => "Abort;" };
+
+        if (instruction.OpCode == OpCodes.Brtrue_S)
+            return new[] { SkipIf(NotEqual(GetLastElementOfVariableStack(0), () => "0"), () => CalcNumActionsToSkip(method, instruction).ToString()) };
+
+        if (instruction.OpCode == OpCodes.Ldc_I4_1)
+            return PushToVariableStack(() => "1");
+
+        if (instruction.OpCode == OpCodes.Sub)
+            return DoBinaryOp(Subtract);
+
+        throw new Exception($"Unsupported opcode {instruction.OpCode}");
+    }
+
+    private static LazyString[] DoBinaryOp(Func<LazyString, LazyString, LazyString> binaryOp)
+    {
+        return new[]
         {
-            if (instruction.OpCode == OpCodes.Ldarg_0)
-                return PushToVariableStack(GetLastElementOfParameterStack(0));
-            if (instruction.OpCode == OpCodes.Ldarg_1)
-                return PushToVariableStack(GetLastElementOfParameterStack(1));
-            if (instruction.OpCode == OpCodes.Ldarg_2)
-                return PushToVariableStack(GetLastElementOfParameterStack(2));
-            if (instruction.OpCode == OpCodes.Ldarg_3)
-                return PushToVariableStack(GetLastElementOfParameterStack(3));
-
-            if (instruction.OpCode == OpCodes.Add)
-                return new[]
-                {
                 // store the last two variables in temp
                 SetGlobal(Variables.Temporary, ArraySubscript(GetGlobal(Variables.VariableStack), Add(GetGlobal(Variables.VariableStackIndex), () => "-1"))),
                 ArrayAppend(Variables.Temporary, ArraySubscript(GetGlobal(Variables.VariableStack), GetGlobal(Variables.VariableStackIndex)))
             }.Concat(
-                    // pop them off the stack
-                    PopStack(Variables.VariableStack, Variables.VariableStackIndex, 2)
-                ).Concat(
-                    // push the addition of them onto the stack
-                    PushToVariableStack(Add(ArraySubscript(GetGlobal(Variables.Temporary), 0), ArraySubscript(GetGlobal(Variables.Temporary), 1)))
-                ).ToArray();
+            // pop them off the stack
+            PopStack(Variables.VariableStack, Variables.VariableStackIndex, 2)
+        ).Concat(
+            // push the addition of them onto the stack
+            PushToVariableStack(binaryOp(ArraySubscript(GetGlobal(Variables.Temporary), 0), ArraySubscript(GetGlobal(Variables.Temporary), 1)))
+        ).ToArray();
+    }
 
-            // TODO: pop parameters off stack and abort?
-            if (instruction.OpCode == OpCodes.Ret)
-                return new LazyString[0];
+    private static int CalcNumActionsToSkip(MethodDefinition method, Instruction instruction)
+    {
+        return method.Body.Instructions.SkipWhile(i => i != instruction).TakeWhile(i => i != instruction.Operand).Sum(i => ToWorkshopActions(method, i).Length) - 1;
+    }
 
-            if (instruction.OpCode == OpCodes.Brtrue_S)
-                return new[] { SkipIf(NotEqual(GetLastElementOfVariableStack(0), () => "0"), () => instruction.Operand.ToString()) };
-
-            throw new Exception($"Unsupported opcode {instruction.OpCode}");
-        }
-
-        static void Main(string[] args)
-        {
-            var source =
+    static void Main(string[] args)
+    {
+        var source =
 @"public static class Workshop {
     public static float Test(float a, float b)
     {
@@ -196,27 +215,26 @@ namespace IL2Workshop
     }
 }";
 
-            var references = new[]
-            {
+        var references = new[]
+        {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
             };
 
-            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimizationLevel: OptimizationLevel.Release);
-            var compilation = CSharpCompilation.Create(
-                "AsmBuild",
-                new[] { SyntaxFactory.ParseSyntaxTree(source) },
-                references,
-                options);
-            var result = compilation.Emit("AsmBuild.dll");
+        var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimizationLevel: OptimizationLevel.Release);
+        var compilation = CSharpCompilation.Create(
+            "AsmBuild",
+            new[] { SyntaxFactory.ParseSyntaxTree(source) },
+            references,
+            options);
+        var result = compilation.Emit("AsmBuild.dll");
 
-            var module = ModuleDefinition.ReadModule("AsmBuild.dll");
-            // var method = module.GetType("Workshop").Methods.First(m => m.Name == "Test");
-            var method = module.GetType("Workshop").Methods.First(m => m.Name == "fib");
-            foreach (var instr in method.Body.Instructions)
-                Console.WriteLine(instr);
-            foreach (var instr in method.Body.Instructions)
-                foreach (var line in ToWorkshopActions(instr))
-                    Console.WriteLine(line());
-        }
+        var module = ModuleDefinition.ReadModule("AsmBuild.dll");
+        // var method = module.GetType("Workshop").Methods.First(m => m.Name == "Test");
+        var method = module.GetType("Workshop").Methods.First(m => m.Name == "fib");
+        foreach (var instr in method.Body.Instructions)
+            Console.WriteLine(instr);
+        foreach (var instr in method.Body.Instructions)
+            foreach (var line in ToWorkshopActions(method, instr))
+                Console.WriteLine(line());
     }
 }
